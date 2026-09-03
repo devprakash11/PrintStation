0bb43b029e325e57648757c0ca80fc69ad3afb72
Dev Prakash <141032391+devprakash11@users.noreply.github.com>
2026-09-03 13:35:38 +0530

    Add reusable footer with brand logo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import BrandLogo from './BrandLogo';
+import { homeData } from '../data/homeData';
+
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="container footer-inner">
+        <BrandLogo />
+        <p>{homeData.footer}</p>
+      </div>
+    </footer>
+  );
+}
