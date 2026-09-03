0cc639698615b25f6c879b6be5672d4cffde05a8
Dev Prakash <141032391+devprakash11@users.noreply.github.com>
2026-09-03 17:49:09 +0530

    refactor: remove printFlow stylesheet import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 import './styles/allPage.css';
-import './styles/printFlow.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
